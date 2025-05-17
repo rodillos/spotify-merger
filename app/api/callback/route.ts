ba55caafd48720e209c024b9d36efac2e6a0975e
Rodrigo <rbotocorbal@gmail.com>
2025-05-16 22:10:02 -0300

    Update route.ts
    added cookies, and refresh token element

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -1,6 +1,7 @@
 // app/api/callback/route.ts
-import { NextRequest, NextResponse } from 'next/server'
-import axios from 'axios'
+import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import axios from 'axios';
 
 const client_id = process.env.SPOTIFY_CLIENT_ID!;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET!;
@@ -9,6 +10,12 @@ const redirect_uri = process.env.NEXT_PUBLIC_REDIRECT_URI!;
 export async function GET(req: NextRequest) {
     const code = req.nextUrl.searchParams.get('code');
     const state = req.nextUrl.searchParams.get('state');
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get('spotify_access_token')?.value
+
+  if (accessToken) {
+    return NextResponse.redirect(`${req.nextUrl.origin}/dashboard?access_token=${accessToken}`);
+  }
 
   if (!code || !state) {
     return NextResponse.redirect(`${req.nextUrl.origin}/?error=missing_code_or_state`)
@@ -31,9 +38,23 @@ export async function GET(req: NextRequest) {
       }
     )
 
-    const { access_token } = response.data;
+    const { access_token, expires_in, refresh_token } = response.data;
 
-    // TODO: averiguar para guardar el token en una cookie
+    cookieStore.set('spotify_access_token', access_token, {
+        maxAge: expires_in,
+        httpOnly: true,
+        secure: true
+    });
+
+    if (refresh_token) {
+      cookieStore.set('spotify_refresh_token', refresh_token, {
+        httpOnly: true,
+        secure: true,
+        path: '/',
+        maxAge: 30 * 24 * 60 * 60, // 30 días
+      })
+    }
+
     return NextResponse.redirect(`${req.nextUrl.origin}/dashboard?access_token=${access_token}`);
   } catch (error) {
     return NextResponse.redirect(`${req.nextUrl.origin}/?error=token_failed`);
